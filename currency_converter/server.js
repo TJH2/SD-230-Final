e2fd71c1d779d345ce195178cf1de06bd119318d
Nevermeant <82971033+TJH2@users.noreply.github.com>
2023-12-06 09:25:56 -0800

    Add files via upload
    added routes for MVC model

--- currency_converter/server.js
+++ currency_converter/server.js
@@ -5,14 +5,26 @@ const port = 5000;
 
 const app = express();
 
-//static files
+// ROUTES
+const indexRouter = require("./routes/index");
+const aboutRouter = require("./routes/about");
+const tableRouter = require("./routes/table");
+const converterRouter = require("./routes/converter");
+const errRouter = require("./routes/404");
+
+// VIEW ENGINE
+app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+
+// STATIC FILES
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
 
+// HELMET
 
 app.use(helmet.contentSecurityPolicy({
   directives: {
@@ -22,6 +34,14 @@ app.use(helmet.contentSecurityPolicy({
 }));
 
 
+app.use("/", indexRouter);
+app.use("/about", aboutRouter);
+app.use("/table", tableRouter);
+app.use("/converter", converterRouter);
+app.use("/*", errRouter);
+
+/*
+OLD ROUTES BEFORE MVC
 app.get("/", (request, response) => {
   response.render("index");
 });
@@ -42,6 +62,8 @@ app.get("/*", (request, response) => {
   response.render("not-found");
 });
 
-app.listen(port, () => {
+*/
+
+app.listen(process.env.PORT || port, () => {
     console.log(`The Program Is Running On Port ${port}`);
 });
